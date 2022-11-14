e2ef7896d81fcc85514812b9979fc63faae277b8
maxusmax2 <79931339+maxusmax2@users.noreply.github.com>
2022-11-14 20:19:22 +0300

    Пересоздание дефолтного проекта

---
+++ BackendBM/BoardMeet/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using BoardMeet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BoardMeet
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<BoardGame> BoardGames { get; set; }
+        public ApplicationContext()
+        {
+            //Database.EnsureDeleted();//Не на релиз!!!!!!!!!!!!!!!!!!!
+            Database.EnsureCreated();
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql(
+                "server=localhost;user=root;password=root;database=boardmeet;",
+                new MySqlServerVersion(new Version(8, 0, 30))
+            );
+        }
+    }
+}
