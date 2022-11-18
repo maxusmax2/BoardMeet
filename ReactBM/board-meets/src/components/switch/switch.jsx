36805ff7a81ede5d0a33168cec1aefb47f0e2cf7
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-11-18 20:30:30 +0300

    Прототип switch

--- ReactBM/board-meets/src/components/switch/switch.jsx
+++ ReactBM/board-meets/src/components/switch/switch.jsx
@@ -1,21 +1,20 @@
 import { useState } from "react";
 import style from "./switch.module.css";
 export const Switch = (props) => {
-    const [check,setCheck] = useState(1);
-
+    const [check,setCheck] = useState("1");
     const radioHandler = (event) => {
         setCheck(event.target.value);
-        {props.radioHandler(check)}
+        {props.radioHandler(event.target.value)}
     }
 
     return(
         <div className = {style.container}>
             <div className = {style.item}>
-                <input type="radio" id="radio-1" name="item" value="1" checked = {check == 1?"checked":null} onClick = {radioHandler}></input>
+                <input type="radio" id="radio-1" name="item" value="1" checked = {check == 1?"checked":null} onChange = {radioHandler}></input>
                 <label htmlFor="radio-1">{props.label1}</label>
             </div>
             <div className = {style.item}>
-                <input type="radio" id="radio-2" name="item" value="2" checked = {check == 2?"checked":null} onClick= {radioHandler}></input>
+                <input type="radio" id="radio-2" name="item" value="2" checked = {check == 2?"checked":null} onChange = {radioHandler}></input>
                 <label htmlFor="radio-2">{props.label2}</label>
             </div>
         </div>
--- ReactBM/board-meets/src/pages/playerPage/playerPage.jsx
+++ ReactBM/board-meets/src/pages/playerPage/playerPage.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 import { MeetCard } from "../../components/meetCard/meetCard";
 import { Switch } from "../../components/switch/switch";
@@ -7,13 +8,18 @@ import style from "./playerPage.module.css";
 
 export const PlayerPage = () => {
     let {id} =useParams();
-    const meets = useData("https://jsonplaceholder.typicode.com/comments?postId=" + id);
+    const [typeMeet,setTypeMeet] = useState({type:"Created",url:"https://jsonplaceholder.typicode.com/comments?postId=1"});
+    const meets = useData(typeMeet.url);
 
     const radioHandler = (value) => {
-        if (value==1){
-            console.log(1);
+        switch(value){
+            case "1":{}
+                setTypeMeet({type:"Created",url:"https://jsonplaceholder.typicode.com/comments?postId=1"});
+                break;
+            case "2":
+                setTypeMeet({type:"Joined",url:"https://jsonplaceholder.typicode.com/comments?postId=2"});
+                break;
         }
-        else  console.log(2);
     }
 
     return(
@@ -22,9 +28,10 @@ export const PlayerPage = () => {
             <Switch label1 = "Created meets" label2 = "Joined meets" radioHandler = {radioHandler}/>
             <ul>
             {!!meets?.length && meets.map((meet) => 
-                <MeetCard meet={meet}/>
+                <li key={meet.id}><MeetCard meet={meet} typeMeet = {typeMeet.type}/></li>
             )}
             </ul>
+           
         </div>
     );
 }
