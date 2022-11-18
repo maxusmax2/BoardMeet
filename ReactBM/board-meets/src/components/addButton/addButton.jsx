942cb967a0f9e1e921b74d6d718c47652473c401
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-11-18 23:32:05 +0300

    Создание компонента AddButton

---
+++ ReactBM/board-meets/src/components/addButton/addButton.jsx
@@ -0,0 +1,10 @@
+import { Plus } from "../icons/icons";
+import style from "./addButton.module.css";
+export const AddButton = () => {
+    
+    return(
+        <div className = {style.button}>
+            <Plus/>
+        </div>
+    );
+}
