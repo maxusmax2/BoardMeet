6ef844bd104e77351bea842d637900e49b83d94f
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-11-18 23:32:21 +0300

    Изменение PlayerPage

--- ReactBM/board-meets/src/pages/playerPage/playerPage.jsx
+++ ReactBM/board-meets/src/pages/playerPage/playerPage.jsx
@@ -4,12 +4,23 @@ import { MeetCard } from "../../components/meetCard/meetCard";
 import { Switch } from "../../components/switch/switch";
 import { UserMainInfo } from "../../components/userMainInfo/userMainInfo";
 import { useData } from "../../hooks/useData";
+import { NavLink } from "react-router-dom";
+import { AddButton } from "../../components/addButton/addButton";
 import style from "./playerPage.module.css";
 
 export const PlayerPage = () => {
     let {id} =useParams();
+
     const [typeMeet,setTypeMeet] = useState({type:"Created",url:"https://jsonplaceholder.typicode.com/comments?postId=1"});
+
     const meets = useData(typeMeet.url);
+
+    let linkAdd =null;
+
+    if (typeMeet.type == "Created"){
+        linkAdd = 
+        <NavLink to = "user/createMeet"> <AddButton/></NavLink>
+    }
 
     const radioHandler = (value) => {
         switch(value){
@@ -26,6 +37,7 @@ export const PlayerPage = () => {
         <div className={style.container}>
             <UserMainInfo id = {id}/>
             <Switch label1 = "Created meets" label2 = "Joined meets" radioHandler = {radioHandler}/>
+            {linkAdd}
             <ul>
             {!!meets?.length && meets.map((meet) => 
                 <li key={meet.id}><MeetCard meet={meet} typeMeet = {typeMeet.type}/></li>
