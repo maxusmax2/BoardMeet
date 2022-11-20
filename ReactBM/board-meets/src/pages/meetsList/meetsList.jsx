a25a709b935aeb3167ed329b8c6084a3f806d496
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-11-20 13:45:31 +0300

    Создание компонента MeetList

--- ReactBM/board-meets/src/pages/meetsList/meetsList.jsx
+++ ReactBM/board-meets/src/pages/meetsList/meetsList.jsx
@@ -1,10 +1,23 @@
+import { Filter } from "../../components/filter/filter";
+import { MeetCard } from "../../components/meetCard/meetCard";
+import { useData } from "../../hooks/useData";
+import style from "./meetsList.module.css";
 
-
-export const MeetsList = () => {
-    
+export const MeetsList = ({user}) => {
+    console.log(user);
+    const meets = useData("http://192.168.1.56:5057/api/Meet");
+      console.log(meets);
     return(
-        <>
-        <p>Meets</p>
-        </>
+        <div className={style.container}>
+            <div className={style.filter}>
+                <Filter/>
+            </div>
+
+            <ul className = {style.meetsList}>
+                {!!meets?.length && meets.map((meet) => 
+                    <li key={meet.id} className={style.meetsItem}><MeetCard meet={meet} typeMeet = {user?.id === meet.authorId?"Created": user?.id === meet.playerId? "Joined": "NonJoined"}/></li>
+                )}
+            </ul>
+        </div>
     );
 }
