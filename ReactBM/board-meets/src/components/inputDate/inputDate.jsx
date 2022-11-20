e0618c5ae90f244f8268b937918fd5605fce9939
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-11-20 16:49:11 +0300

    Изменение InputDate

--- ReactBM/board-meets/src/components/inputDate/inputDate.jsx
+++ ReactBM/board-meets/src/components/inputDate/inputDate.jsx
@@ -1,8 +1,10 @@
+import { Date } from "../icons/icons";
 import style from "./inputDate.module.css";
 export const InputDate = () => {
     
     return(
         <div className={style.container}>
+            <label className={style.inputIcon}><Date/></label>
             <input type="date" className={style.date}></input>
         </div>
     );
