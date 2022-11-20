2560063f062204cba62ce24b1a7c2aed69563964
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-11-20 13:44:28 +0300

    Создан компонент PlayerPage

--- ReactBM/board-meets/src/pages/playerPage/playerPage.jsx
+++ ReactBM/board-meets/src/pages/playerPage/playerPage.jsx
@@ -1,49 +1,48 @@
 import { useState } from "react";
-import { useParams } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
+import { AddButton } from "../../components/addButton/addButton";
 import { MeetCard } from "../../components/meetCard/meetCard";
 import { Switch } from "../../components/switch/switch";
-import { UserMainInfo } from "../../components/userMainInfo/userMainInfo";
 import { useData } from "../../hooks/useData";
-import { NavLink } from "react-router-dom";
-import { AddButton } from "../../components/addButton/addButton";
 import style from "./playerPage.module.css";
 
 export const PlayerPage = () => {
-    let {id} =useParams();
 
-    const [typeMeet,setTypeMeet] = useState({type:"Created",url:"https://jsonplaceholder.typicode.com/comments?postId=1"});
+    let {userId} = useParams();
+
+    const [typeMeet,setTypeMeet] = useState({type:"Created",url:"http://192.168.1.56:5057/api/User/CreatedMeet/"+userId});
 
     const meets = useData(typeMeet.url);
 
     let linkAdd =null;
 
-    if (typeMeet.type == "Created"){
+    if (typeMeet.type === "Created"){
         linkAdd = 
-        <NavLink to = "user/createMeet"> <AddButton/></NavLink>
+        <NavLink to ={ `/user/${userId}/createMeet`}> <AddButton/></NavLink>
     }
 
     const radioHandler = (value) => {
         switch(value){
             case "1":{}
-                setTypeMeet({type:"Created",url:"https://jsonplaceholder.typicode.com/comments?postId=1"});
+                setTypeMeet({type:"Created",url:"http://192.168.1.56:5057/api/User/CreatedMeet/"+userId});
                 break;
             case "2":
-                setTypeMeet({type:"Joined",url:"https://jsonplaceholder.typicode.com/comments?postId=2"});
+                setTypeMeet({type:"Joined",url:"http://192.168.1.56:5057/api/User/JoinedMeet/"+userId});
                 break;
         }
     }
-
     return(
-        <div className={style.container}>
-            <UserMainInfo id = {id}/>
-            <Switch label1 = "Created meets" label2 = "Joined meets" radioHandler = {radioHandler}/>
+        <>
+            <div className={style.switch}>
+                <Switch label1 = "Created meets" label2 = "Joined meets" radioHandler = {radioHandler}/>
+            </div>
+
             {linkAdd}
-            <ul>
-            {!!meets?.length && meets.map((meet) => 
-                <li key={meet.id}><MeetCard meet={meet} typeMeet = {typeMeet.type}/></li>
-            )}
+            <ul className = {style.meetsList}>
+                {!!meets?.length && meets.map((meet) => 
+                    <li key={meet.id} className={style.meetsItem}><MeetCard meet={meet} typeMeet = {typeMeet.type}/></li>
+                )}
             </ul>
-           
-        </div>
+        </>
     );
 }
