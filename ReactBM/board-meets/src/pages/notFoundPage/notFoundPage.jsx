56f2d152d9726387faaf37bd0bacdd34a559400b
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-11-22 22:18:49 +0300

    Начало создания NotFoundPage

---
+++ ReactBM/board-meets/src/pages/notFoundPage/notFoundPage.jsx
@@ -0,0 +1,11 @@
+
+import style from "./notFoundPage.module.css";
+
+export const NotFoundPage = () => {
+    
+    return(
+        <div className={style.container}>
+            <div className={style.decoration}></div>
+        </div>
+    );
+}
