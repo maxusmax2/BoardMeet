17c32da682fda745a630fe136b0e007a6fb27238
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-11-22 22:19:07 +0300

    Заглушка регистрации

---
+++ ReactBM/board-meets/src/pages/registration/registration.jsx
@@ -0,0 +1,10 @@
+import style from "./registration.module.css";
+
+export const Registration = () => {
+    
+    return(
+        <div className={style.container}>
+            <p>Registration</p>
+        </div>
+    );
+}
