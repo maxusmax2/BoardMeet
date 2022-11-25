71fad14a981684c0f626183bc543a3172f0fa320
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-11-25 09:09:18 +0300

    Изменение методов для входа/регистрации и выхода

--- ReactBM/board-meets/src/App.jsx
+++ ReactBM/board-meets/src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import { MainLayout } from "./layouts/mainLayout/mainLayout";
 import { GamesList } from "./pages/gamesList/meetsList";
 import { LogIn } from "./pages/logIn/logIn";
@@ -10,24 +10,35 @@ import { OrganizationPage } from "./pages/organizationPage/organizationPage";
 import { NotFoundPage } from "./pages/notFoundPage/notFoundPage";
 import { CreateMeetPage } from "./pages/createMeetPage/createMeetPage";
 import { Registration } from "./pages/registration/registration";
-
+import Cookies from 'universal-cookie';
 
 export const App = () => {
-
-  const [user,setUser] = useState(null);
-  const [token,setToken] = useState(null);
+  
+  const cookies = new Cookies();
+  const [user,setUser] = useState(cookies.get('user')=="0"?parseInt(cookies.get('user')):cookies.get('user'));
+  const [token,setToken] = useState(cookies.get('token')=="0"?parseInt(cookies.get('token')):cookies.get('token'));
   
   const url = "http://25.43.153.92:5057/api/";
 
   const buttonHandler = (user,token) => {
-      setUser(user);
-      setToken(token);
+    cookies.set('user', user, { path: '/' });
+    cookies.set('token', token, { path: '/' });
+    console.log(cookies.get('user')); 
+    setUser(cookies.get('user'));
+    setToken(cookies.get('token'));
   }
- 
+  const exitHandler = () => {
+    cookies.set('user', 0, { path: '/' });
+    cookies.set('token', 0, { path: '/' });
+    setUser(cookies.get('user'));
+    setToken(cookies.get('token'));
+    window.location.reload()
+  }
+
   return (
     <>
       <Routes>
-        <Route path="/" exact element={<MainLayout user = {user}/>}>
+        <Route path="/" exact element={<MainLayout user = {user} exitHandler={exitHandler}/>}>
           <Route path="/" element={<MeetsList userId = {user?.id} url = {url} role = {user?.role}/>}/>
           <Route path="/games" element={<GamesList/>}/>
           <Route path="/user/:userId" element={<UserPageLayout url = {url}/>}>
@@ -40,7 +51,7 @@ export const App = () => {
           </Route>
         </Route>
         <Route path="/logIn" element={<LogIn buttonHandler={buttonHandler} url = {url}/>}></Route>
-        <Route path="/registration" element={<Registration/>}></Route>
+        <Route path="/registration" element={<Registration url={url} buttonHandler={buttonHandler}/>}></Route>
         <Route path="*" element={<NotFoundPage/>} />
       </Routes>
     </>
