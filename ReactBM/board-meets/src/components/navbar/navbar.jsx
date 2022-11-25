87e5b40b6d22ab7360d7c6b4f0cd8e5b39820eab
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-11-25 09:13:52 +0300

    Реализация выхода из системы

--- ReactBM/board-meets/src/components/navbar/navbar.jsx
+++ ReactBM/board-meets/src/components/navbar/navbar.jsx
@@ -3,16 +3,16 @@ import { NavLink } from "react-router-dom";
 import { Exit, Logo, Search, User} from "../icons/icons";
 
 
-export const Navbar = ({user}) => {
-    let buttonLink = <NavLink to = "/logIn" className = {style.logInButton}>LogIn</NavLink>;
-
+export const Navbar = ({user,exitHandler}) => {
+    console.log(user);
+    let buttonLink = <NavLink to = "/logIn" className = {style.logInButton}>Войти</NavLink>;
     if (user){
         buttonLink = 
         <div className={style.buttons}>
             <NavLink to = {`user/${user.id}/${user.role}`} className = {style.profileButton}>
                 <User/>
             </NavLink> 
-            <button type ="button" className = {style.exitButton} ><Exit/></button>
+            <button type ="button" className = {style.exitButton} onClick={exitHandler}><Exit/></button>
         </div>
             
     }
@@ -23,13 +23,13 @@ export const Navbar = ({user}) => {
                 <Logo/>
             </div>
             <div className = {style.search}>
-                <input type="search" className = {style.searchInput} placeholder="Search your lover game"></input>
+                <input type="search" className = {style.searchInput} placeholder="Найдите свою любимую игру"></input>
                 <button type="button" className = {style.searchButton}><Search/></button>
             </div>
             
             <div className={style.linkList}>
-                <NavLink to = "/" className={style.linkListLink} >Meets</NavLink>
-                <NavLink to = "/games" className={style.linkListLink}>Games</NavLink>
+                <NavLink to = "/" className={style.linkListLink} >Мероприятия</NavLink>
+                <NavLink to = "/games" className={style.linkListLink}>Игры</NavLink>
             </div>
 
             {buttonLink}       
