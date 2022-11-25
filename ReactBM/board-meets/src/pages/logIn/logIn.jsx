ddb2f807b3c222aa5f5333180e1f7169d6526b88
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-11-25 09:08:14 +0300

    Изменение компонента LogIN
    Добавление валидации

--- ReactBM/board-meets/src/pages/logIn/logIn.jsx
+++ ReactBM/board-meets/src/pages/logIn/logIn.jsx
@@ -5,15 +5,15 @@ import { useForm } from 'react-hook-form';
 import axios from "axios";
 
 export const LogIn = ({buttonHandler,url}) => {
-    const {register,handleSubmit} = useForm();
+    const {register, handleSubmit, formState:{errors}} = useForm();
     const navigate = useNavigate();
    
     const onSubmit = (data) => {
         const body = {email:data.email, password:data.password};
-        axios.post(url + "User/Authorize",body)
+        axios.post(url + "User/Authorize", body)
         .then((response) => {
             console.log(response.data);
-            buttonHandler(response.data.authUser,response.data.token);
+            buttonHandler(response.data.authUser, response.data.token);
         })
         .then(()=>navigate("/"))
         .catch((err) => {
@@ -27,36 +27,44 @@ export const LogIn = ({buttonHandler,url}) => {
                 console.log("c");
             } 
         });
-        
 
     };
 
-
     return(
-        <div className={style.container}>
-            <div className={style.decoration}></div>
-            <div className={style.formContainer}>
-                <div className={style.title}>
-                    <p className={style.mainTitle}>Board Meets</p>
-                    <p className={style.subTitle}>for your fun)</p>
+        <div className = {style.container}>
+            <div className = {style.decoration}></div>
+            <div className = {style.formContainer}>
+                <div className = {style.title}>
+                    <p className = {style.mainTitle}>Board Meets</p>
+                    <p className = {style.subTitle}>for your fun)</p>
                 </div>
-                <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
-                    <p className={style.formTitle}>LogIn</p>
-                    <div className={style.formInput}>
+                <form className = {style.form} onSubmit = {handleSubmit(onSubmit)}>
+                    <p className = {style.formTitle}>Вход</p>
+                    <div className = {style.formInput}>
 
-                        <label htmlFor="email" className={style.mandatoryLabel}>Mandatory</label>
-                        <input type="email" id="email" name="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" className={style.input} placeholder = " " {...register("email")} required></input>
-                        <label className={style.swimLabel} htmlFor="email">Email</label>
+                        <label className = {style.mandatoryLabel}>Обязательно</label>
+                        <input type = "email"  className = {style.input} placeholder = " " {...register("email",{
+                            required:"введите свой email",
+                            pattern:{
+                                value:/[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+                                message: "email введ некорректно"
+                                } 
+                            })}/>
+                        {errors?.email&&<p className = "error">{errors.email.message}</p>}
+                        <label className = {style.swimLabel}>Email</label>
                     </div>
-                    <div  className={style.formInput}>
-                        <label htmlFor="password" className={style.mandatoryLabel}>Mandatory</label>
-                        <input type="password" name="password" className={style.input}  placeholder = " " {...register("password")} required></input>
-                        <label className={style.swimLabel}  htmlFor="password" >Password</label>
+                    <div className = {style.formInput}>
+                        <label className = {style.mandatoryLabel}>Обязательно</label>
+                        <input type = "password" className = {style.input}  placeholder = " " {...register("password",{
+                            required:"введите свой пароль"
+                            })}/>
+                        {errors?.password&&<p className = "error">{errors.password.message}</p>}
+                        <label className = {style.swimLabel}>Пароль</label>
                     </div>
-                    <input type="submit" className={style.formButton} value="Continue"></input>
+                    <input type="submit" className = {style.formButton} value="Вход"></input>
                     <div className={style.links}> 
-                        <NavLink to="/" className={style.navLink}><Back/>Continue without registration</NavLink>
-                        <NavLink to="/registration" className={style.navLink}><Write fill="black" width="13" height="13"/>Registration</NavLink>
+                        <NavLink to="/" className = {style.navLink}><Back/>Продолжить без регистрации</NavLink>
+                        <NavLink to="/registration" className = {style.navLink}><Write fill = "black" width = "13" height = "13"/>Регистрация</NavLink>
                     </div>
                 </form>
             </div>
