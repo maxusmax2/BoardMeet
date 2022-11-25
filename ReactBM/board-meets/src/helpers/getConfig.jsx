6ae61634c05f999e789daf53e5704112ba2dd62b
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-11-25 09:14:21 +0300

    Создание помощников по получению  из куки токена и юзера

--- ReactBM/board-meets/src/helpers/getConfig.jsx
+++ ReactBM/board-meets/src/helpers/getConfig.jsx
@@ -1,4 +1,6 @@
+import Cookies from 'universal-cookie';
 export const getConfig = () => {
-    const token =  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Im1heHVzbWF4MkBnbWFpbC5jb20iLCJyb2xlIjoicGxheWVyIiwibmJmIjoxNjY5MjI1OTUxLCJleHAiOjE2NjkyMjk1NTEsImlhdCI6MTY2OTIyNTk1MX0.7YxgTpWAjJ0YJi7zbNndyPETZcCNJxjnvu2I6LgPeww";
+    let cookies = new Cookies();
+    let token = cookies.get('token');
     return ({headers : {Authorization: `Bearer ${token}`}});
 };
---
+++ ReactBM/board-meets/src/helpers/getUser.jsx
@@ -0,0 +1,6 @@
+import Cookies from 'universal-cookie';
+export const getUser = () => {
+    let cookies = new Cookies();
+    let user = cookies.get('user');
+    return (user);
+};
