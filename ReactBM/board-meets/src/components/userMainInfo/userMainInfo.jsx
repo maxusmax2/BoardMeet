5e51c61a35656a5f93ba41fddfa343d27c700409
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-12-17 23:46:45 +0300

    Добавление в UserMainInfo смены аватара

--- ReactBM/board-meets/src/components/userMainInfo/userMainInfo.jsx
+++ ReactBM/board-meets/src/components/userMainInfo/userMainInfo.jsx
@@ -1,49 +1,55 @@
 import style from "./userMainInfo.module.css";
 import { useDataGet } from "../../hooks/useDataGet";
 import { useState } from "react";
-import { FileInput } from "../fileInput/fileInput";
+import { AvatarInput } from "../avatarInput/avatarInput";
 import { City, EditAvatar, Email, UserName } from "../icons/icons";
+import { getUser } from "../../helpers/getUser";
 
-export const UserMainInfo = ({id,url}) => {
-    const [editAvatar,setEditAvatar] = useState(false);
-    const user = useDataGet(url + "User/" + id)
+export const UserMainInfo = ({ id, url }) => {
+	const [editAvatar, setEditAvatar] = useState(false);
+	const user = useDataGet(url + "Users/" + id)
+	
+	let fileInput = null;
+	let btnEdit = null;
 
-    let fileInput = null;
-    const editAvatarInputHandler =()=>{
-        setEditAvatar(!editAvatar);
-    }
-    if (editAvatar){
-        fileInput = <FileInput/>
-    }
-    
-   
-    return(
-        <div className={style.mainContainer}>
-            <img src = {url + user?.avatarUrl} alt = "userPhoto" className={style.avatar}/>
-            <div className={style.container}>
-                <button type="button" className={style.editAvatarButton} onClick={()=>{editAvatarInputHandler()}}><EditAvatar/></button>
-                <div className={style.userInfoContainer}>
-                    <ul className={style.userNameStatus}>
-                        <li className={style.userName}>@{user?.userName}</li>
-                        <li className={style.userStatus}>{user?.aboutMe}</li>
-                        <li>{fileInput}</li>
-                    </ul>
-                    <ul className={style.userData}>
-                        <li className={style.userDataItem}>
-                            <UserName/>
-                             <p className={style.dataItemText}>{user?.name}</p> 
-                        </li>
-                        <li className={style.userDataItem}>
-                            <Email/>
-                            <p className={style.dataItemText}>{user?.email}</p>
-                        </li>
-                        <li className={style.userDataItem}>
-                            <City/>
-                            <p className={style.dataItemText}>г.{user?.city}</p>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    );
+	const editAvatarInputHandler = () => {
+		setEditAvatar(!editAvatar);
+	}
+	if (user?.id == getUser()?.id){
+		btnEdit = 	<button type="button" className={style.editAvatarButton} onClick={() => { editAvatarInputHandler() }}><EditAvatar /></button>;
+	}
+	if (editAvatar) {
+		fileInput = <AvatarInput userId={id} url={url}/>
+	}
+
+
+	return (
+		<div className={style.mainContainer}>
+			<img src={url + user?.avatarUrl} alt="userPhoto" className={style.avatar} />
+			<div className={style.container}>
+				{btnEdit}
+				<div className={style.userInfoContainer}>
+					<ul className={style.userNameStatus}>
+						<li className={style.userName}>@{user?.userName}</li>
+						<li className={style.userStatus}>{user?.aboutMe}</li>
+						<li>{fileInput}</li>
+					</ul>
+					<ul className={style.userData}>
+						<li className={style.userDataItem}>
+							<UserName />
+							<p className={style.dataItemText}>{user?.name}</p>
+						</li>
+						<li className={style.userDataItem}>
+							<Email />
+							<p className={style.dataItemText}>{user?.email}</p>
+						</li>
+						<li className={style.userDataItem}>
+							<City />
+							<p className={style.dataItemText}>г.{user?.city}</p>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 }
