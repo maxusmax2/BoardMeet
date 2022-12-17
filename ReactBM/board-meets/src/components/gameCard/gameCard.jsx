5d0af7d763ab2deb1ca0e4c82ae0e06466db7556
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-12-17 23:54:05 +0300

    Изменение компонента GameCard
    Удаление заглушек + рефакторинг

--- ReactBM/board-meets/src/components/gameCard/gameCard.jsx
+++ ReactBM/board-meets/src/components/gameCard/gameCard.jsx
@@ -1,25 +1,29 @@
 import { GameMainInfo } from "../gameMainInfo/gameMainInfo";
-import { Age, Change, Genre, Players, TimeBold, Write } from "../icons/icons";
+import { Change } from "../icons/icons";
 import style from "./gameCard.module.css";
-import { useNavigate} from "react-router-dom";
-export const GameCard = ({ meet, url, userId, role }) => {
-    const navigate = useNavigate();
-    const readHandler = () => {
-        navigate("/game/1");
-    }
-    return (
-        
+import { NavLink, useNavigate } from "react-router-dom";
+import { getUser } from "../../helpers/getUser";
+export const GameCard = ({ game, url }) => {
+  const user = getUser();
+  const navigate = useNavigate();
+  const readHandler = () => {
+    navigate("/game/" + game.id);
+  }
+  const changeHandler = () => {
+    navigate(`/user/${user.id}/changeGame/${game.id}`);
+  }
+  return (
 
-            <div className={style.container}>
-               <GameMainInfo/>
-                <div className={style.moreInfo}>
-                    <p className={style.author}>@gaga_games</p>
-                    <button className={style.buttonChange}><Change width="35" height="35" /></button>
-                    <p className={style.shortDesription}>Итак, представьте, что вы являетесь мэром крошечного городка в лесной чаще, что расположен в самом сердце маленькой страны, надёжно укрытой от хищников. К сожалению, земли здесь недостаточно, да и ресурсов маловато...Итак, представьте, что вы являетесь мэром крошечного городка в лесной чаще, что расположен в самом сердце маленькой страны, надёжно укрытой от хищников. К сожалению, земли здесь недостаточно, да и ресурсов маловато...</p>
-                    <button className={style.buttonRead} onClick={readHandler}>Read</button>
-                </div>
+    <div className={style.container}>
+      <GameMainInfo game={game} url={url} />
+      <div className={style.moreInfo}>
+        <NavLink className={style.author} to = {game.authorId==user.id?`/user/${game.authorId}/${user.role}`:`/user/${game.authorId}`}>@{game.author.userName}</NavLink>
+        <button className={style.buttonChange} hidden={game.authorId == user.id ? false : true} onClick={changeHandler}><Change width="35" height="35" /></button>
+        <p className={style.shortDesription}>{game.description}</p>
+        <button className={style.buttonRead} onClick={readHandler}>Read</button>
+      </div>
 
-            </div>
-       
-    );
+    </div>
+
+  );
 }
