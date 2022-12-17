47847813180bc5fc3a11decf403ca0bc01f962ff
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-12-17 23:43:52 +0300

    Изменение MeetList
    Рефакторинг + добавление фильтра

--- ReactBM/board-meets/src/pages/meetsList/meetsList.jsx
+++ ReactBM/board-meets/src/pages/meetsList/meetsList.jsx
@@ -1,21 +1,40 @@
-import { Filter } from "../../components/filter/filter";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FilterMeets } from "../../components/filterMeets/filterMeets";
 import { MeetCard } from "../../components/meetCard/meetCard";
+import { getConfig } from "../../helpers/getConfig";
 import { useDataGet } from "../../hooks/useDataGet";
 import style from "./meetsList.module.css";
 
-export const MeetsList = ({userId,url,role}) => {
-    const meets = useDataGet(url + "Meet");
-    return(
-        <div className={style.container}>
-            <div className={style.filter}>
-                <Filter/>
-            </div>
+export const MeetsList = ({ url }) => {
+  const [meets,setMeets] = useState();
+  const { handleSubmit, register } = useForm();
 
-            <ul className = {style.meetsList}>
-                {!!meets?.length && meets.map((meet) => 
-                    <li key={meet.id} className={style.meetsItem}><MeetCard url = {url} meet={meet} userId={userId} role={role}/></li>
-                )}
-            </ul>
-        </div>
-    );
+  let meetList = useDataGet(url + "Meets");
+  useEffect(() => {
+    if (meetList) {
+      setMeets(meetList);
+    }
+  }, [meetList]);
+
+  const searchHandler = (data) => {
+    let body = data.date?{city: data.city,date: data.date}:{city: data.city};
+    axios.post(url + "Meets/Search", body, getConfig())
+    .then((response) => {
+      setMeets(response.data);
+     ;
+    })
+  }
+  return (
+    <div className={style.container}>
+      <FilterMeets register={register} handleSubmit={handleSubmit} searchHandler={searchHandler}/>
+      <ul className={style.meetsList}>
+        {!!meets?.length && meets.map((meet) =>
+          <li key={meet.id} className={style.meetsItem}><MeetCard url={url} meet={meet} /></li>
+        )}
+      </ul>
+    </div>
+  );
 }
