3330d176590e27f9edc044283980de58ea688d45
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-12-17 23:53:07 +0300

    Изменение компонента GameMainInfo
    Удаление заглушек + рефакторинг

--- ReactBM/board-meets/src/components/gameMainInfo/gameMainInfo.jsx
+++ ReactBM/board-meets/src/components/gameMainInfo/gameMainInfo.jsx
@@ -1,31 +1,30 @@
-import { Age, Change, Genre, Players, TimeBold, Write } from "../icons/icons";
+import { Age, Genre, Players, TimeBold } from "../icons/icons";
 import style from "./gameMainInfo.module.css";
-export const GameMainInfo = () => {
-
-    return (
-        <div className={style.mainInfo}>
-            <img className={style.gameImg} src="assets/images/background.png" alt="game" />
-            <div className={style.gameInfo}>
-                <p className={style.gameName}>Tiny Town</p>
-                <ul>
-                    <li className={style.gameItem}>
-                        <p className={style.gameItemInfo}>2-5</p>
-                        <Players />
-                    </li>
-                    <li className={style.gameItem}>
-                        <p className={style.gameItemInfo}>90+ минут</p>
-                        <TimeBold />
-                    </li>
-                    <li className={style.gameItem}>
-                        <p className={style.gameItemInfo}>13+</p>
-                        <Age />
-                    </li>
-                    <li className={style.gameItem}>
-                        <p className={style.gameItemInfo}>Card Game</p>
-                        <Genre />
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
+export const GameMainInfo = ({game, url}) => {
+	return (
+		<div className={style.mainInfo}>
+			<img className={style.gameImg} src={url + game?.gameAvatar} alt="game" />
+			<div className={style.gameInfo}>
+				<p className={style.gameName}>{game?.name}</p>
+				<ul>
+					<li className={style.gameItem}>
+						<p className={style.gameItemInfo}>{game?.rangeOfPlayersMin}-{game?.rangeOfPlayersMax}</p>
+						<Players />
+					</li>
+					<li className={style.gameItem}>
+						<p className={style.gameItemInfo}>{game?.gameTime}+ минут</p>
+						<TimeBold />
+					</li>
+					<li className={style.gameItem}>
+						<p className={style.gameItemInfo}>{game?.agePlayer}+</p>
+						<Age />
+					</li>
+					<li className={style.gameItem}>
+						<p className={style.gameItemInfo}>{game?.genre}</p>
+						<Genre />
+					</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
