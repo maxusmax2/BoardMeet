031cc879b7aa46ea7395b4f85692162afca1e9f7
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-12-20 22:52:35 +0300

    Изменение атрибута иконки Write

--- ReactBM/board-meets/src/components/nameInput/nameInput.jsx
+++ ReactBM/board-meets/src/components/nameInput/nameInput.jsx
@@ -4,7 +4,7 @@ export const NameInput = ({ register, errors }) => {
   return (
     <>
       <div className={style.container}>
-        <label htmlFor="age"><Write width="20" height="20" fill="#057402" />
+        <label htmlFor="age"><Write width="20" height="20" stroke="#057402" />
           <p>Название</p>
         </label>
         <input type="text" id="name" placeholder="Щеночки-вперед" className={errors?.name ? style.inputError : style.input}{...register("name", {
