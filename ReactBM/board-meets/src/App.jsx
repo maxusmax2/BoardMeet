e33a51d425a319f0cec43e173c0f4b02862a9f8a
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-12-20 23:01:36 +0300

    Изменение роута на "admin"

--- ReactBM/board-meets/src/App.jsx
+++ ReactBM/board-meets/src/App.jsx
@@ -1,7 +1,7 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes,  Navigate } from "react-router-dom";
 import { MainLayout } from "./layouts/mainLayout/mainLayout";
 import { LogIn } from "./pages/logIn/logIn";
-import { MeetsList } from "./pages/meetsList/meetsList";
+import { MeetsListPage } from "./pages/meetsListPage/meetsListPage";
 import { UserPageLayout } from "./layouts/userPageLayout/userPageLayout.jsx";
 import { PlayerPage } from "./pages/playerPage/playerPage";
 import { OrganizationPage } from "./pages/organizationPage/organizationPage";
@@ -15,27 +15,31 @@ import { GamePage } from "./pages/gamePage/gamePage";
 import { PublisherPage } from "./pages/publisherPage/publisherPage";
 import { CreateGamePage } from "./pages/createGamePage/createGamePage";
 import { ChangeGamePage } from "./pages/changGamePage/changeGamePage";
+import { useState } from "react";
+import { getUser } from "./helpers/getUser";
 
 export const App = () => {
   const cookies = new Cookies();
+  const [user,setUser] = useState();
 
   const url = "http://25.43.153.92:5057/api/";
 
   const buttonHandler = (user,token) => {
     cookies.set('user', user, { path: '/' });
     cookies.set('token', token, { path: '/' });
+    setUser(getUser());
   }
   const exitHandler = () => {
     cookies.set('user', 0, { path: '/' });
     cookies.set('token', 0, { path: '/' });
-    window.location.reload()
+    setUser(getUser());
   }
 
   return (
     <>
       <Routes>
         <Route path="/" exact element={<MainLayout exitHandler={exitHandler}/>}>
-          <Route path="/" element={<MeetsList url = {url} />}/>
+          <Route path="/" element={<MeetsListPage url = {url} />}/>
           <Route path="/games" element={<GamesList url={url}/>}/>
           <Route path="/search/:gameName" element={<GamesList url={url}/>}/>
           <Route path="/game/:gameId" element={<GamePage url={url}/>}/>
@@ -47,7 +51,7 @@ export const App = () => {
             <Route path="createGame" element={<CreateGamePage url ={url}/>}/>
             <Route path="changeGame/:gameId" element={<ChangeGamePage url={url}/>}/>
             <Route path="publisher" element={<PublisherPage url={url}/>}/>
-            <Route path="admin" element={<MeetsList/>}/>
+            <Route path="admin" element={<Navigate to={`/user/${user?.id}`} />}/>
           </Route>
         </Route>
         <Route path="/logIn" element={<LogIn buttonHandler={buttonHandler} url = {url}/>}></Route>
