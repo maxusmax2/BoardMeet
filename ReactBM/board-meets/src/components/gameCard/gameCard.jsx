21b7a03cc72c84c95d03cd20c630a028930fc71b
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-12-20 23:01:08 +0300

    Изменение компонента GameCard
    -создание кнопки удаления

--- ReactBM/board-meets/src/components/gameCard/gameCard.jsx
+++ ReactBM/board-meets/src/components/gameCard/gameCard.jsx
@@ -1,24 +1,39 @@
 import { GameMainInfo } from "../gameMainInfo/gameMainInfo";
-import { Change } from "../icons/icons";
+import { Change, MinusButton } from "../icons/icons";
 import style from "./gameCard.module.css";
 import { NavLink, useNavigate } from "react-router-dom";
 import { getUser } from "../../helpers/getUser";
-export const GameCard = ({ game, url }) => {
+import axios from "axios";
+import { getConfig } from "../../helpers/getConfig";
+export const GameCard = ({ game, url, delGame }) => {
   const user = getUser();
   const navigate = useNavigate();
+  const delHandler = () =>{
+    axios.delete(url + "BoardGames/" + game.id, getConfig())
+      .then(() =>delGame(game))
+      .catch((err) => {
+        if (err.response) { console.log("a"); }
+        else if (err.request) { console.log("b"); }
+        else { console.log("c"); }
+      });
+  }
+  
   const readHandler = () => {
     navigate("/game/" + game.id);
   }
   const changeHandler = () => {
     navigate(`/user/${user.id}/changeGame/${game.id}`);
   }
+  const button = user?.role == "admin"? <button className={style.buttonDel} onClick={delHandler}> <MinusButton fill="#64C661"/></button>: 
+  <button className={style.buttonChange} hidden={game.authorId == user?.id? false : true} onClick={changeHandler}><Change width="35" height="35" /></button>
+  
   return (
 
     <div className={style.container}>
       <GameMainInfo game={game} url={url} />
       <div className={style.moreInfo}>
-        <NavLink className={style.author} to = {game.authorId==user.id?`/user/${game.authorId}/${user.role}`:`/user/${game.authorId}`}>@{game.author.userName}</NavLink>
-        <button className={style.buttonChange} hidden={game.authorId == user.id ? false : true} onClick={changeHandler}><Change width="35" height="35" /></button>
+        <NavLink className={style.author} to = {game.authorId==user?.id?`/user/${game.authorId}/${user?.role}`:`/user/${game.authorId}`}>@{game.author.userName}</NavLink>
+        {button}
         <p className={style.shortDesription}>{game.description}</p>
         <button className={style.buttonRead} onClick={readHandler}>Read</button>
       </div>
