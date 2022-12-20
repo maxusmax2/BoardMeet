ee979922d4df5d6c010535298d819902cd61ea30
Vivi <100605523+FawnVivin@users.noreply.github.com>
2022-12-20 22:46:15 +0300

    Изменение компонента meetMainInfo
    -Изменение верстки аватарки
    -Внедрение MeetIndicator в MeetMainInfo

--- ReactBM/board-meets/src/components/meetMainInfo/meetMainInfo.jsx
+++ ReactBM/board-meets/src/components/meetMainInfo/meetMainInfo.jsx
@@ -2,13 +2,14 @@ import style from "./meetMainInfo.module.css";
 import { getMonthDay, getTime } from "../../helpers/date";
 import { getUser } from "../../helpers/getUser";
 import { NavLink } from "react-router-dom";
+import { MeetIndicator } from "../meetIndicate/meetIndicator";
 export const MeetMainInfo = ({ moreInfoHandler, meet, url }) => {
   const user = getUser();
   let games = JSON.parse(meet.games).join(", ");
   return (
     <section className={style.mainInfo}>
       <div className={style.userInfo}>
-        <NavLink to={meet.authorId==user.id?`/user/${user.id}/${user.role}`:`/user/${meet.authorId}`} className={style.userName}>@{meet.author.userName}</NavLink>
+        <NavLink to={meet.authorId==user?.id?`/user/${user?.id}/${user?.role}`:`/user/${meet.authorId}`} className={style.userName}>@{meet.author.userName}</NavLink>
         <img src={url + meet.author.avatarUrl} alt="userPhoto" className={style.userPhoto}></img>
       </div>
       <div className={style.textInfo} onClick={moreInfoHandler}>
@@ -17,7 +18,7 @@ export const MeetMainInfo = ({ moreInfoHandler, meet, url }) => {
           <p className={style.location}> г. {meet.city} | {meet.location}</p>
           <p className={style.time}>{getTime(meet.date)}</p>
         </div>
-        <div className={style.verticalLine}></div>
+        <MeetIndicator meet={meet}/>
         <div className={style.textInfoItem}>
           <p className={style.name}>{meet.name}</p>
           <p className={style.games}> {games}</p>
